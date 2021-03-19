fb265c73a1965413fcdff6daf7335af22a77ee46
Louis Cloutier <louis.cloutier@shopify.com>
2021-03-19 05:59:06 -0400

    Add zsh-autosuggestions plugin
    Signed-off-by: Louis Cloutier <louis.cloutier@shopify.com>

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ mv $HOME/.zshrc $HOME/.zshrc_backup
 cp .zshrc $HOME/.zshrc
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 echo "Installing powerlevel10k..."
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
